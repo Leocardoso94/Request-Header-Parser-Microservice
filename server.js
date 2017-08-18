a0320d71ca19a76d22733e70854f9b872a59de00
System <none>
2017-08-18 16:48:17 +0000

    🍑👐 Updated with Glitch

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var app = express();
 
+const port = process.env.PORT || 8080;
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -12,28 +14,19 @@ var app = express();
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function (req, res) {
+  const ip = req.headers['x-forwarded-for'].split(',')[0];;
+  const language = req.headers["accept-language"].split(',')[0];
+  const software = req.headers["user-agent"].match(/\(([^)]+)\)/)[0].replace(/\(|\)/g, '');
+  
+  res.json({
+    ipaddress: ip,
+    language: language,
+    software: software
+ });
 });
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
